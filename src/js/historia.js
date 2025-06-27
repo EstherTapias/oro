bc713413ee56b272d12be73f5b580fdc12faff5e
EstherTapias <esthertpc93@gmail.com>
2025-06-27 14:34:44 +0200

    implement common elements: footer and nav(#1)

--- src/js/historia.js
+++ src/js/historia.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+  fetch('../index.html')
+  .then(response => response.text())
+  .then(data => {
+    // Extrai o header
+    const headerMatch = data.match(/<header[^>]*>([\s\S]*?)<\/header>/i);
+    if (headerMatch) {
+      document.getElementById('header-nave').innerHTML = headerMatch[1];
+      runLogoAnimation(); // chama a animação depois de inserir o header
+    }
+
+    // Extrai o footer
+    const footerMatch = data.match(/<footer[^>]*>([\s\S]*?)<\/footer>/i);
+    if (footerMatch) {
+      document.getElementById('footer').innerHTML = footerMatch[1];
+    }
+  });
   // --- Mapa Leaflet ---
   const mapa = L.map("mapa").setView([20, 0], 2);
 
