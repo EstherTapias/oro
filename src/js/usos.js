041be650e83a58e0ceb286b6ea8390b4706924d5
juliazmor <juliezarco21@gmail.com>
2025-06-29 15:58:28 +0200

    added onmouseover event for images and small animation (#5)

--- src/js/usos.js
+++ src/js/usos.js
@@ -1,20 +1,26 @@
 sunPic.addEventListener("click", function (eventInfo) {
-    //Busca todas las imágenes que están visibles y no visibles
-    let imagenes = document.querySelectorAll(".img-not-visible, .img-visible");
+  //Busca todas las imágenes que están visibles y no visibles
+  let imagenes = document.querySelectorAll(".img-not-visible, .img-visible");
 
-    //Recorre todas las imágenes encontradas
-    imagenes.forEach(img => {
-        //Si la imagen está oculta...
-        if (img.classList.contains("img-not-visible")) {
-            //La muestra
-            img.classList.remove("img-not-visible");
-            img.classList.add("img-visible");
-        } else {
-            //Si está visible... La oculta
-            img.classList.remove("img-visible");
-            img.classList.add("img-not-visible");
-        }
+  //Recorre todas las imágenes encontradas
+  imagenes.forEach(img => {
+    //Si la imagen está oculta...
+    if (img.classList.contains("img-not-visible")) {
+      //La muestra
+      img.classList.remove("img-not-visible");
+      img.classList.add("img-visible");
+      img.classList.add("animate__animated");
+      img.classList.add("animate__bounceIn");
+    } else {
+      //Si está visible... La oculta
+      img.classList.remove("img-visible");
+      img.classList.add("img-not-visible");
+    }
+    //Agregar evento onmouseover a las imágenes
+    img.addEventListener("mouseover", function () {
+      alert("mostrar la flip-card!");
     });
+  });
 });
 
 fetch('../index.html')
